da7b86b9969e2951f666d01ab5dd5cd8b129e11a
Javier Galarza <jegj57@gmail.com>
2024-12-16 07:11:35 -0500

    chore: demo and trying to use random sprite

--- src/main.ts
+++ src/main.ts
@@ -6,60 +6,91 @@ const k = kaplay({
 });
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadBean("bean");
+k.loadSpriteAtlas('./public/sprites/kenney_pixel-line-platformer/Tilemap/tilemap_packed.png', {
+  hero: {
+    x: 16,
+    y: 16,
+    width: 160,
+    height: 96,
+  }
+});
 k.setGravity(1600);
 
 k.scene("game", () => {
-  const rotatingBean = k.add([
-    k.sprite("bean"),
-    k.pos(50, 100),
-    k.area(),
-    k.anchor("center"),
-    k.rotate(0),
-    k.animate(),
-    k.body()
-  ]);
+  // const level = k.addLevel([
+  //   // Design the level layout with symbols
+  //   "@  ^ $$",
+  //   "=======",
+  // ], {
+  //   // The size of each grid
+  //   tileWidth: 64,
+  //   tileHeight: 64,
+  //   // The position of the top left block
+  //   pos: k.vec2(100, 200),
+  //   // Define what each symbol means (in components)
+  //   tiles: {
+  //     "@": () => [
+  //       k.sprite("bean"),
+  //       k.area(),
+  //       k.body(),
+  //       k.anchor("bot"),
+  //       "player",
+  //     ],
+  //     "=": () => [
+  //       k.sprite("grass"),
+  //       k.area(),
+  //       k.body({ isStatic: true }),
+  //       k.anchor("bot"),
+  //     ],
+  //     "$": () => [
+  //       k.sprite("coin"),
+  //       k.area(),
+  //       k.anchor("bot"),
+  //       "coin",
+  //     ],
+  //     "^": () => [
+  //       k.sprite("spike"),
+  //       k.area(),
+  //       k.anchor("bot"),
+  //       "danger",
+  //     ],
+  //   },
+  // });
 
-  rotatingBean.animate("angle", [0, 360], {
-    duration: 2,
-    direction: "forward",
-  });
-
-  // const movingBean = k.add([
+  // const rotatingBean = k.add([
   //   k.sprite("bean"),
-  //   k.pos(150, 100),
+  //   k.pos(50, 100),
   //   k.area(),
   //   k.anchor("center"),
+  //   k.rotate(0),
   //   k.animate(),
   //   k.body()
   // ]);
-  //
-  // // This will animate the bean from left to right in 2 seconds
-  // // The direction "ping-pong" means that when it goes to the right, it will move back to the left
-  // movingBean.animate("pos", [k.vec2(150, 100), k.vec2(200, 100)], {
-  //   duration: 2,
-  //   direction: "ping-pong",
+
+
+  //   direction: "forward",
   // });
+  //
+  k.add([
+    k.sprite("hero"),
+    k.pos(k.center()),
+    k.anchor("center"),
+    k.area(),
+    k.body(),
+  ]);
+
+  // player.play("idle");
+
+
   k.add([
     k.rect(k.width(), 50),
     k.area(),
     k.outline(3),
-    k.pos(0, 150),
+    k.pos(0, 650),
     k.body({
       isStatic: true,
     })
   ]);
-  k.onKeyPress("space", () => {
-    // .isGrounded() is provided by body()
-    if (rotatingBean.isGrounded()) {
-      // .jump() is provided by body()
-      rotatingBean.jump();
-    }
-  });
-
-  rotatingBean.onGround(() => {
-    k.debug.log("ouch");
-  });
-
 
 });
 k.go("game");
